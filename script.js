c65456f76ccc4ffedfc59c157c3ba3a1aa90d0a7
Ifeoluwa Adeyemi <aifeoluwadiadem@gmail.com>
2022-08-23 21:49:44 +0100

    task 1-21

---
+++ script.js
@@ -0,0 +1,106 @@
+// Task 1
+
+// happy ending
+
+/* new beginning */
+
+// Task 2
+var myName;
+
+//Task3
+var a;
+a = 7;
+
+//Task 4
+var a;
+a = 7;
+var b;
+b = a;
+
+//Task 5
+var a = 9;
+
+// Task 6
+var myFirstName = 'Ifeoluwa';
+var myLastName = 'Oni';
+
+//Task 7
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Task 8
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver= 9000;
+
+//Task 9
+const FCC = 'freeCodeCamp'; // Change this line
+let fact = 'is cool!'; // Change this line
+fact = 'is awesome!';
+console.log(FCC, fact); 
+
+//Task 10
+const sum = 10 + 10;
+
+//Task 11
+const difference = 45 - 33;
+
+//Task 12
+const product = 8 * 10;
+
+//Task 13
+const quotient = 66 / 33;
+
+//Task 14
+let myVar = 87;
+// Only change code below this line
+myVar++;
+
+//Task 15
+let myVar = 11;
+// Only change code below this line
+myVar--;
+
+// Task 16
+const myDecimal = 7.4;
+
+// Task 17
+const product = 2.0 * 2.5;
+
+// Task 18
+const quotient = 4.4 / 2.0;
+
+// Task 19
+const remainder = 11 % 3;
+
+// Task 20
+let a = 3;
+let b = 17;
+let c = 12;
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+// Task 21
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
