48d4247ae21fa10c835f1cb87509644e29249db7
Ifeoluwa Adeyemi <aifeoluwadiadem@gmail.com>
2022-08-24 20:47:10 +0100

    task 22 - 39

---
+++ file2.js
@@ -0,0 +1,101 @@
+// Task 22
+let a = 5;
+let b = 12;
+let c = 4.6;
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+// Task 23
+let a = 48;
+let b = 108;
+let c = 33;
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+//Task 24
+const myStr = 'I am a "double quoted" string inside "double quotes".';
+
+//Task 25
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// Task 26
+const myStr = 'FirstLine\n\t\\SecondLine\nThirdLine';
+
+// Task 27
+const myStr = 'This is the start. ' + 'This is the end.';
+
+// Task 28
+let myStr = 'This is the first sentence. ';
+myStr += 'This is the second sentence.';
+
+// Task 29
+const myName = 'Ifeoluwa';
+const myStr = 'My name is ' + myName + 'and I am well!';
+
+// Task 30
+let lastNameLength = 0;
+const lastName = 'Lovelace';
+// Only change code below this line
+lastNameLength = lastName.length;
+
+// Task 31
+let firstLetterOfLastName = '';
+const lastName = 'Lovelace';
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
+
+// Task 32
+let myStr = 'Jello World';
+// Only change code below this line
+myStr = 'Hello World'; // Change this line
+
+// Task 33
+// Setup
+const lastName = 'Lovelace';
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2];
+
+// Task 34
+const lastName = 'Lovelace';
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1];
+
+// Task 35
+const lastName = 'Lovelace';
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
+// Task 36
+const myNoun = 'dog';
+const myAdjective = 'big';
+const myVerb = 'ran';
+const myAdverb = 'quickly';
+
+// Only change code below this line
+const wordBlanks =
+  'My ' +
+  myAdjective +
+  ' fat ' +
+  myNoun +
+  ' had to ' +
+  myVerb +
+  ' swiftly ' +
+  myAdverb +
+  ' across the road';
+
+// Task 37
+const myArray = ['letter', 24];
+
+// Task 38
+const myArray = [
+  ['Gbenro', 30],
+  ['Ife', 26],
+];
+
+// Task 39
+const myArray = [50, 60, 70];
+const myData = myArray[0];
