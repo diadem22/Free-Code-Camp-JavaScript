f1f12a85c2e7736c559c59f74c9b95d099ebf37a
Ifeoluwa Adeyemi <aifeoluwadiadem@gmail.com>
2022-10-24 21:09:18 +0100

    intermediate algorithm scripting(task 13  - 16)

--- Intermediate Algorithm Scripting/file2.js
+++ Intermediate Algorithm Scripting/file2.js
@@ -34,13 +34,66 @@ function sumFibs(num) {
 
 sumFibs(4);
 
-// Task
+// Task 13: Sum All Primes
+function sumPrimes(num) {
+  function isPrime(num) {
+    for (let i = 2; i < num; i++)
+      if (num % i === 0) {
+        return false;
+      }
+    return true;
+  }
+  let total = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) total += i;
+  }
+  return total;
+}
 
-// Task
+sumPrimes(10);
 
-// Task
+// Task 14:Smallest Common Multiple
+function smallestCommons(arr) {
+  const [min, max] = arr.sort((a, b) => a - b);
+  const range = Array(max - min + 1)
+    .fill(0)
+    .map((a, i) => i + min);
 
-// Task
+  const HighestMultiple = range.reduce((product, current) => product * current);
+
+  for (let multiple = max; multiple <= HighestMultiple; multiple += max) {
+    const divisible = range.every((value) => multiple % value === 0);
+    if (divisible) {
+      return multiple;
+    }
+  }
+}
+
+smallestCommons([1, 5]);
+
+// Task 15: Drop it
+function dropElements(arr, func) {
+  while (arr.length > 0 && !func(arr[0])) {
+    arr.shift();
+  }
+  return arr;
+}
+
+dropElements([1, 2, 3], function (n) {
+  return n < 3;
+});
+
+// Task 16: Steamroller
+function steamrollArray(arr, flattenedArr = []) {
+  arr.forEach((item) => {
+    if (Array.isArray(item)) {
+      steamrollArray(item, flattenedArr);
+    } else flattenedArr.push(item);
+  });
+  return flattenedArr;
+}
+
+steamrollArray([1, [2], [3, [[4]]]]);
 
 // Task
 
